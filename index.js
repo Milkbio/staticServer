3e5befe91307250d9b7ac6e69fa3ef398eeca1d1
honest <1032635701@qq.com>
2020-02-02 15:44:16 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const koa = require('koa');
+const logger = require('koa-logger');
+const koastatic = require('koa-static');
+const chalk = require('chalk');
+
+
+const app = new koa();
+app.use(logger());
+
+// 静态资源文件
+app.use(koastatic(path.resolve(__dirname, 'dist')));
+
+
+const port = 3000;
+
+app.listen(port);
+console.log(chalk.green(`listening on port ${port}`));
